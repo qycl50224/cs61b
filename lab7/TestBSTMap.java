9db8779048492532c502654ace84a4d844b90565
qycl50224 <qy50224@163.com>
2020-02-20 23:51:12 +0800

    finished lab7

--- lab7/TestBSTMap.java
+++ lab7/TestBSTMap.java
@@ -23,6 +23,7 @@ public class TestBSTMap {
         for (int i = 0; i < 455; i++) {
             b.put("hi" + i, 1+i);
             //make sure put is working via containsKey and get
+            b.printInOrder();
             assertTrue( null != b.get("hi" + i) && (b.get("hi"+i).equals(1+i))
                         && b.containsKey("hi" + i));
         }
