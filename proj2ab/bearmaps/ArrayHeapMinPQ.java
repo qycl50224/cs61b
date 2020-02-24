563e09f59f38cc1c1469c094962fac73f9c44ab6
qycl50224 <qy50224@163.com>
2020-02-25 00:00:08 +0800

    finished ArrayHeapMinPQ Demo

---
+++ proj2ab/bearmaps/ArrayHeapMinPQ.java
@@ -0,0 +1,24 @@
+package bearmaps;
+
+public class ArrayHeapMinPQ<T> implements ExtrinsicMinPQ<T> {
+
+
+    @Override
+    public void add(T item, double priority) {}
+    /* Returns true if the PQ contains the given item. */
+    @Override
+    public boolean contains(T item) {}
+    /* Returns the minimum item. Throws NoSuchElementException if the PQ is empty. */
+    @Override
+    public T getSmallest() {}
+    /* Removes and returns the minimum item. Throws NoSuchElementException if the PQ is empty. */
+    @Override
+    public T removeSmallest() {}
+    /* Returns the number of items in the PQ. */
+    @Override
+    public int size() {}
+    /* Changes the priority of the given item. Throws NoSuchElementException if the item
+     * doesn't exist. */
+    @Override
+    public void changePriority(T item, double priority) {}
+}
