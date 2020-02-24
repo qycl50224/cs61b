36455351522fef655cbfc453b5459ef8f171fe75
qycl50224 <qy50224@163.com>
2020-02-25 01:47:48 +0800

    writed the helper func and the node

--- proj2ab/bearmaps/ArrayHeapMinPQ.java
+++ proj2ab/bearmaps/ArrayHeapMinPQ.java
@@ -1,22 +1,56 @@
 package bearmaps;
 
+import java.util.ArrayDeque;
+
 public class ArrayHeapMinPQ<T> implements ExtrinsicMinPQ<T> {
+    private node[] pq;
+    private int size;
+    public ArrayHeapMinPQ(int size) {
+        this.size = size;
+        pq = (node[]) new Object[size+1];
+    }
 
+    public class node {
+        private T item;
+        private double priority;
+        public node(T item, double priority) {
+            this.item = item;
+            this.priority = priority;
+        }
+    }
 
+    private void less() {}
+    private void exch() {}
+
+    /* Adds an item with the given priority value. Throws an
+     * IllegalArgumentExceptionb if item is already present.
+     * You may assume that item is never null. */
     @Override
-    public void add(T item, double priority) {}
+    public void add(T item, double priority) {
+        for (node n: pq) {
+            if (n.item == item) {
+                throw new IllegalArgumentException();
+            }
+        }
+        node n = new node(item, priority);
+    }
     /* Returns true if the PQ contains the given item. */
     @Override
     public boolean contains(T item) {}
     /* Returns the minimum item. Throws NoSuchElementException if the PQ is empty. */
     @Override
-    public T getSmallest() {}
+    public T getSmallest() {
+        return pq[1].item;
+    }
     /* Removes and returns the minimum item. Throws NoSuchElementException if the PQ is empty. */
     @Override
     public T removeSmallest() {}
     /* Returns the number of items in the PQ. */
     @Override
-    public int size() {}
+    public int size() {
+        return size;
+    }
+
     /* Changes the priority of the given item. Throws NoSuchElementException if the item
      * doesn't exist. */
     @Override
