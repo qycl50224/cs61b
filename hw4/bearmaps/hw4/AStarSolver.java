a7130c470127cd3dd3d43d70bd29b4dd9cd8d9e4
qycl50224 <qy50224@163.com>
2020-03-04 14:58:14 +0800

    add Stack of edgeTo so we can construct a pathTo function for solution

--- hw4/bearmaps/hw4/AStarSolver.java
+++ hw4/bearmaps/hw4/AStarSolver.java
@@ -3,13 +3,14 @@ package bearmaps.hw4;
 import bearmaps.proj2ab.ArrayHeapMinPQ;
 import java.util.HashMap;
 import edu.princeton.cs.algs4.Stopwatch;
-
+import java.util.Stack;
 import java.util.ArrayList;
 import java.util.List;
 
 public class AStarSolver<Vertex> implements ShortestPathsSolver<Vertex> {
     private ArrayHeapMinPQ<Vertex> PQ;
     private HashMap<Vertex, Double> distTo;
+    private HashMap<Vertex, WeightedEdge<Vertex>> edgeTo;
     private SolverOutcome outcome;
     private int numStatesExplored;
     private double solutionWeight;
@@ -30,6 +31,7 @@ public class AStarSolver<Vertex> implements ShortestPathsSolver<Vertex> {
         solution = new ArrayList<Vertex>();
         distTo = new HashMap<>();
         distTo.put(start, 0.);
+        edgeTo = new HashMap<>();
 
         PQ.add(start, distTo.get(start));
         while (PQ.size() != 0) {
@@ -42,7 +44,7 @@ public class AStarSolver<Vertex> implements ShortestPathsSolver<Vertex> {
              * and return
              */
             if (v.equals(end)) {
-                solution.add(v);
+                solution = pathTo(v);
                 outcome = SolverOutcome.SOLVED;
                 explorationTime = sw.elapsedTime();
                 solutionWeight = distTo.get(v);
@@ -53,12 +55,16 @@ public class AStarSolver<Vertex> implements ShortestPathsSolver<Vertex> {
              * the core algorithms going on here
              */
             for (WeightedEdge<Vertex> e: input.neighbors(v)) {
+                /*
+                 * as hashMap itself didn't contains the key, so when we meet
+                 * a new key we should put it in and initiate it's distance to
+                 * INF
+                 */
                 if (!distTo.containsKey(e.to())) {
                     distTo.put(e.to(), INF);
                 }
                 relax(e, input, end);
             }
-            solution.add(v);
         }
 
         outcome = SolverOutcome.UNSOLVABLE;
@@ -79,12 +85,26 @@ public class AStarSolver<Vertex> implements ShortestPathsSolver<Vertex> {
         double w = e.weight();
         if (distTo.get(q) > distTo.get(p) + w) {
             distTo.replace(q, distTo.get(p) + w);
+            edgeTo.put(q,e);
             if (PQ.contains(q)) {
                 PQ.changePriority(q, distTo.get(q) + input.estimatedDistanceToGoal(q,goal));
             } else {
                 PQ.add(q, distTo.get(q) + input.estimatedDistanceToGoal(q,goal));
             }
         }
+    }
+
+    private List<Vertex> pathTo(Vertex v) {
+        Stack<Vertex> path = new Stack<>();
+        for (WeightedEdge<Vertex> e = edgeTo.get(v); e != null; e = edgeTo.get(e.from())) {
+            path.push(e.from());
+        }
+        List<Vertex> truePath = new ArrayList<>();
+        while(!path.isEmpty()) {
+            truePath.add(path.pop());
+        }
+        truePath.add(v);
+        return truePath;
     }
 
     @Override
